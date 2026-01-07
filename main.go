edd7f49c9d0a422b01bd6a5c58643683e5b13c27
Ioannis Tsiros <jtsiros93@gmail.com>
2026-01-07 17:43:25 +0100

    working on starting the crawler

--- main.go
+++ main.go
@@ -2,8 +2,32 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+	"os"
 )
 
+func run() error {
+	if len(os.Args) < 2 {
+		return fmt.Errorf("no website provided")
+	}
+	if len(os.Args) > 2 {
+		return fmt.Errorf("too many arguments provided")
+	}
+
+	fmt.Println("starting crawl of:", os.Args[1])
+	return nil
+}
+
+func getHTML(rawURL string) (string, error) {
+	req, err := http.NewRequest("GET", rawURL, nil)
+	if err != nil {
+		return "", err
+	}
+}
+
 func main() {
-	fmt.Println("Hello, World!")
+	if err := run(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
 }
